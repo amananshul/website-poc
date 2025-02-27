d3141c6a905b06b75737b3c2e9e5f5fa0e890268
Aman dubey <aman004dubey@gmail.com>
2025-02-27 19:23:07 +0530

    Fix component issues

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/aman/website-poc/node_modules/infima/dist/css/default/default.css"),
+  require("/home/aman/website-poc/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/aman/website-poc/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/aman/website-poc/src/css/custom.css"),
+];
